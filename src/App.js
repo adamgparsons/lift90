f6580ba339b8500debb8a880a9760e17a510ef7a
adamgparsons <adamgeorgeparsons@gmail.com>
2019-07-24 11:22:59 +0100

    added meta tag

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./styles/App.css";
 import "./styles/tachyons.min.css";
 import FormInput from "./FormInput";
 import Results from "./Results";
+import MetaTags from "react-meta-tags";
 
 function App() {
   const [OneRepMax, setOneRepMax] = useState("");
   return (
     <React.Fragment>
+      <MetaTags>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </MetaTags>
       <div className="bar" />
       <div className="lato mw-400 center ph2-5">
         <h1 className="white normal mt4 mt5-ns f3 f2-ns ">
